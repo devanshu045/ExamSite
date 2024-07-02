980d1f378045e4aed0426114a307221ab2dea8c1
devanshu045 <Sharmadev9664431714@gmail.com>
2024-07-02 23:54:48 +0530

    Exam Creatation

--- backend/Controller/TeacherController.js
+++ backend/Controller/TeacherController.js
@@ -1,9 +1,9 @@
 const TeacherDetailsModel = require('../Models/TeacherDetails'); 
+const QuestionsModel = require('../Models/QuestionsDetails');
 
 
 exports.teacherSign =   (req, res) => {
     console.log(req.body);
-    
     TeacherDetailsModel.create(req.body)
         .then(() => {
             res.status(200).send("Data stored in database");
@@ -18,25 +18,43 @@ exports.teacherLogin = async  (req, res) => {
     try {
         console.log(req.body);
         const { Email, password } = req.body;
-    
         if (!Email) {
             // throw new Error('Email is missing in req.body');
             console.log("user not found 2")
         }
-    
         const finduser = await TeacherDetailsModel.findOne({ email: Email});
-    
-
         if (!finduser) {
             console.log('User not found');
         } else {
             console.log('User found:', finduser);
         }
-    
     } catch (error) {
         console.error('Error in find user:', error);
     }
-    
 }
 
+
+exports.teacherAddQuestions = async (req,res) => {
+    console.log(req.body)
+    try{
+        for(let i=0; i<req.body.length; i++){
+            QuestionsModel.create(req.body[i]).then(() => {
+                res.status(200).send("Data stored in database");
+            })
+            .catch((err) => {
+                console.error("Error storing data:", err);
+                res.status(500).send("Failed to store data");
+            });
+        }
+    }
+    catch (error) {
+        console.error('Error in find user:', error);
+    }
+
+}
+
+
+exports.CreateExam = (req,res) =>{
+
+}
 
--- backend/Routes/TeacherRoutes.js
+++ backend/Routes/TeacherRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { teacherSign, teacherLogin } = require('../Controller/TeacherController');
+const { teacherSign, teacherLogin, teacherAddQuestions, CreateExam } = require('../Controller/TeacherController');
 
 const router = express.Router();
 
 router.post('/Signin', teacherSign);
 router.post('/Login', teacherLogin)
+// router.post('/AddQuestions', teacherAddQuestions)
+router.post('/CreateExam',CreateExam)
 // Define more routes here as needed
 
 module.exports = router;
+
